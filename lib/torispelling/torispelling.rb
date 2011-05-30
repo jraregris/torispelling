967bf3bbcf7b0467e70c4e385192a905601ff827
Oddmund Strømme <oddmund@oddmundo.com>
2011-05-30 04:02:50 +0200

    Haha! It works.

--- lib/torispelling/torispelling.rb
+++ lib/torispelling/torispelling.rb
@@ -4,7 +4,13 @@ require 'pp'
 class Object
   def method_missing method, *args, &block
     sym = methods.sort_by{ |m| Text::Levenshtein.distance method.to_s, m.to_s}.first
-    send(sym.to_s) if Text::Levenshtein.distance(sym, method) < 10
+    dist = Text::Levenshtein.distance method.to_s, sym.to_s
+
+    if dist < 2
+      send(sym.to_s)
+    else
+      super
+    end
   end
 end
 
--- test/torispelling_test.rb
+++ test/torispelling_test.rb
@@ -4,7 +4,7 @@ require 'test/unit'
 require 'torispelling'
 
 class ToriSpellingTest < Test::Unit::TestCase
-  def test_string_srot
+  def test_revers
     s = "STRING"
     assert s.revers == s.reverse
   end
